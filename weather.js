b66402857efd1f5c5035987a7b9fc346e467d813
Coldstrike30 <coldstrike34@gmail.com>
2026-02-16 15:08:15 +0100

    yes

--- weather.js
+++ weather.js
@@ -22,7 +22,7 @@ let weekdayName =[];
 async function fetchData(query) {
     try{
        
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=3ece03829e6540d6a27101023260602&q=${query}&days=7&aqi=no&alerts=no`);
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3ece03829e6540d6a27101023260602&q=${query}&days=7&aqi=no&alerts=no`);
         const data = await response.json();
 
         return data;
